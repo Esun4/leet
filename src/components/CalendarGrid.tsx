fdd916f464b8db81ebfc9d52921bc5b4b643767d
Esun4 <ethansun44@gmail.com>
2026-06-25 00:30:11 -0400

    created grid function logic

---
+++ src/components/CalendarGrid.tsx
@@ -0,0 +1,10 @@
+import type { Problem } from '../types/index';
+
+type CalendarGridProps = {
+    problemsByDate: Record<string, Problem[]>;
+
+};
+
+export default function CalendarGrid({ problemsByDate }: CalendarGridProps) {
+    return;
+}
--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { getProblems } from '../lib/problems';
 import type { Problem } from '../types/index';
-
+import CalendarGrid from '../components/CalendarGrid';
 
 export default function CalendarPage() {
     const [problems, setProblems] = useState<Problem[]>([]);
@@ -14,5 +14,13 @@ export default function CalendarPage() {
         fetchProblems();
     }, [])
 
-    return <div>Problems: {problems.length} </div>
+    const problemsByDate = problems.reduce((acc, problem) => {
+    if (!acc[problem.next_review]) {
+        acc[problem.next_review] = [];
+    }
+    acc[problem.next_review].push(problem);
+    return acc;
+    }, {} as Record<string, Problem[]>);
+
+    return <CalendarGrid problemsByDate={problemsByDate} />;
 }
